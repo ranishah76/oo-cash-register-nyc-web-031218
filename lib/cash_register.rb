5c41d6278b52d9f509a0e2b36e315dacd45c0054
Rani Shah <rani76@gmail.com>
2018-03-19 01:28:45 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -21,8 +21,7 @@ def apply_discount
   if @discount > 0
   @total = @total*(100 - @discount)/100
    "After the discount, the total comes to $#{total}."
-end
+ else
    "There is no discount to apply."
-end
-
+  end
 end
