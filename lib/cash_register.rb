85b47531a4fd49fc5f5cc9c6318e5e34bb282683
Rani Shah <rani76@gmail.com>
2018-03-19 01:29:13 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,7 @@ def add_item(title, price, quantity = 1)
 end
 
 def apply_discount
-  if @discount > 0
+if @discount > 0
   @total = @total*(100 - @discount)/100
    "After the discount, the total comes to $#{total}."
  else
