d038acf92962979d2aaa07a39f5af1159d04ba5c
Rani Shah <rani76@gmail.com>
2018-03-19 01:32:50 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,8 +14,10 @@ end
 
 def add_item(title, price, quantity = 1)
   self.total += price*quantity
-  quantity.times do 
+  quantity.times do
   @items << title
+end
+  self.last_total = total 
 end
 
 def apply_discount
