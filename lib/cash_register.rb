79fa9f5f887603303ffdf6cc01461a3c48819954
Rani Shah <rani76@gmail.com>
2018-03-19 01:19:43 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,11 +1,11 @@
 class CashRegister
 
-attr_accessor :item, :price, :total
+attr_accessor :item, :price, :total, :discount 
 
 def initialize
   @item = item
   @price = price
-  @discount = discount 
+  @discount = discount
   @total = 0
 end
 
